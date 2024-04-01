7c37413414bf481f9a453e3230f384a83b78b0d7
CostiCatargiu <constantin.catargiu@yahoo.com>
2024-04-01 22:03:34 +0300

    Add YOLO models

--- YoloLib/Yolov8/ExportONNX.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO('/home/constantin/Doctorat/YoloModels/Yolov8/runs/detect/new_yolov8m_train6/weights/best_80epochs.pt')  # load a custom trained model
-
-# Export the model
-model.export(format='onnx')
